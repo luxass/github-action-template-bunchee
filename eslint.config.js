82c829890cf4ae18a75c3c988b86a3b6b5511931
Lucas <lucasnrgaard@gmail.com>
2025-10-28 03:02:41 +0100

    chore: switch eslint setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,11 @@
 // @ts-check
-import { luxass } from "@luxass/eslint-config";
 
-export default luxass({
-  formatters: true,
-});
+import eslint from "@eslint/js";
+import { defineConfig, globalIgnores } from "eslint/config";
+import tseslint from "typescript-eslint";
+
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  globalIgnores(["dist/*"])
+)
